f3d561bbe5f7a98f8b783ee449466157668b948a
BENJYY1337 <pernot.benjamin@outlook.fr>
2020-11-25 15:28:33 +0100

    save message on hooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Formulaire from './components/Formulaire'
 import Message from './components/Message'
 
-const App = () => {
+const App = (props) => {
+
+  const [message, setMessage] = useState({})
+  const [pseudo, setPseudo] = useState(props.match.params.pseudo)
+
+  const addMessage = message => {
+    const messages = { ...messages }
+    messages[`message-${Date.now()}`] = message
+    setMessage({ messages })
+  }
+
   return (
     <div className='box'>
       <div>
         <div className="messages">
           <Message />
+          <Message />
+          <Message />
         </div>
       </div>
-      <Formulaire />
+      <Formulaire
+        length={140}
+        pseudo={pseudo}
+        addMessage={addMessage} />
     </div>
   )
 }
--- src/components/Formulaire.js
+++ src/components/Formulaire.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Formulaire = () => {
+const Formulaire = (props) => {
+
+  const [message, setMessage] = useState({})
+
+  const createMessage = () => {
+    // console.log("d", props)
+    // const { addMessage, pseudo } = props
+
+    props.addMessage(message)
+
+    // Reset
+    setMessage({})
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    createMessage()
+  }
+
+  const handleChange = event => {
+    const { name, value } = event.target
+    setMessage((m) => ({ ...m, [name]: value }))
+  }
+
   return (
     <div>
-      <form className="form">
-        <textarea required maxLength='140' />
+      <form className="form" onSubmit={handleSubmit}>
+        <textarea value={message.message ? message.message : ""} name='message' onChange={handleChange} placeholder='message' required maxLength='140' />
         <div className='info'>140</div>
         <button type='submit'>Envoyer!</button>
       </form>
